c364d208e5a267ec16ba11ae9f233f3cad95c18c
Artem Kononovich <artem.kononovich4@gmail.com>
2017-09-21 19:31:22 +0300

    zeros: done

--- src/index.js
+++ src/index.js
@@ -1,46 +1,20 @@
 module.exports = function zeros(exp) {
-  exp = exp.split("*");
-  var k, mult;
-  for (let i = 0; i < exp.length; i++) {
-    k = (exp[i][exp[i].length - 2] == "!") ? 2 : 1;
-    exp[i] = exp[i].slice(0, exp[i].length - k);
-    for (let j = exp[i] - k; j > 1; j -= k) {
-      exp[i] = multiply(exp[i], j.toString());
-    }
-  }
-  mult = exp[0];
-  for (let i = 1; i < exp.length; i++) {
-    mult = multiply(mult, exp[i]);
-  }
-  for (i = mult.length - 1; i > 0 && mult[i] == "0"; i--);
-  return mult.length - i - 1;
-}
-
-function multiply(a, b) {
-  if (a == 0 || b == 0) return "0";
-  a = a.split("");
-  b = b.split("");
-  a = a.reverse();
-  b = b.reverse();
-  if (a.length > b.length) {
-    t = a;
-    a = b;
-    b = t;
-  }
-  var n = a.length, m = b.length, p = new Array(m * 2);
-  for (let i = 0; i < p.length; i++) p[i] = 0;
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < m; j++) {
-      p[i + j] += a[i] * b[j];
-    }
-  }
-  for (let i = 0; i < p.length; i++) {
-    if (p[i] > 9) {
-      p[i + 1] += Math.floor(p[i] / 10);
-      p[i] = p[i] % 10;
-    }
-  }
-  for (let i = p.length - 1; i > 0 && p[i] == 0; i--) p.pop();
-  p = p.reverse();
-  return p.join("");
+	exp = exp.split("*");
+	var n = exp.length, number2 = 0, number5 = 0;
+	for (let i = 0; i < n; i++) {
+		let k = (exp[i][exp[i].length - 2] == "!") ? 2 : 1;
+		exp[i] = parseInt(exp[i]);
+		for (let j = exp[i]; j > 1; j -= k) {
+			let temp = j
+			while (temp % 5 == 0) {
+				number5++;
+				temp /= 5;
+			}
+			while (temp % 2 == 0) {
+				number2++;
+				temp /= 2;
+			}
+		}
+	}
+	return Math.min(number2, number5);
 }
